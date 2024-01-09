0bf36f46b31c7c377aaec6bd2b035e096e26a0b7
Departamento de Sistemas <sistemas@azc.uam.mx>
2024-01-09 11:26:25 -0600

    Init proyect

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://Jose-Eduardo-Hrz.github.io',
+	integrations: [
+		starlight({
+			title: 'LPOO',
+			tableOfContents: { minHeadingLevel: 2, maxHeadingLevel: 5 },
+			sidebar: [
+				{
+					label: 'Notas',
+					autogenerate: { directory: 'notas' },
+				},
+				{
+					label: 'Tareas',
+					autogenerate: { directory: 'tareas' },
+				},
+			],
+		}),
+	],
+});
