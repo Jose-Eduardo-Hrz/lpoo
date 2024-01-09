e0470c4941544f2c88870758b025b3bb60ccf235
Departamento de Sistemas <sistemas@azc.uam.mx>
2024-01-09 13:03:03 -0600

    add url site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'lpoo.eduardo-uam.com',
+	site: 'https://lpoo.eduardo-uam.com/',
 	base: '/lpoo',
 	integrations: [
 		starlight({
